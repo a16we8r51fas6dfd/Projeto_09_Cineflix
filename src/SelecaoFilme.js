d92def9c6c70ece0b0f543a8fc27c9ec05ca4dea
fernandinho <fernando_yudi_yaeda@hotmail.com>
2021-12-19 17:43:35 -0300

    escolha de filme 1/2

--- src/SelecaoFilme.js
+++ src/SelecaoFilme.js
@@ -1,6 +1,25 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
 import './SelecaoFilme.css'
 
 export default function SelecaoFilme() {
+    const [filmes, setFilmes] = useState(null)
+    
+    useEffect(() => {
+        const requisicao = axios.get("https://mock-api.driven.com.br/api/v4/cineflex/movies")
+
+        requisicao.then(resposta => {
+            setFilmes(resposta.data)
+        })
+    }, [])
+
+    if(filmes === null) {
+        return(
+            <h1>carregando carregando carregando carregando carregando carregando carregando carregando</h1>
+        )
+    }
+
     return(
         <>
             <header>
@@ -12,30 +31,11 @@ export default function SelecaoFilme() {
             </div>
 
             <div className='filmes'>
-                <div className="filme">
-
-                </div>
-                <div className="filme">
-                    
-                </div>
-                <div className="filme">
-                    
-                </div>
-                <div className="filme">
-                    
-                </div>
-                <div className="filme">
-                    
-                </div>
-                <div className="filme">
-                    
-                </div>
-                <div className="filme">
-                    
-                </div>
-                <div className="filme">
-                    
-                </div>
+                {filmes.map((filme) => 
+                    <div className="filme">
+                        <img src={filme.posterURL} alt="img" />
+                    </div>
+                )}
             </div>
         </>
     )
--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
 import ReactDOM from 'react-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import "./css/reset.css"
 import "./css/style.css"
 import SelecaoFilme from './SelecaoFilme'
-import SelecaoHorario from './SelecaoHorario'
-import SelecaoAssentos from './SelecaoAssentos'
-import Sucesso from './Sucesso'
+/* import SelecaoHorario from './SelecaoHorario' */
+/* import SelecaoAssentos from './SelecaoAssentos'
+import Sucesso from './Sucesso' */
 
 function App() {
+
     return(
-        <>
-            <Sucesso />
-        </>
+        <BrowserRouter>
+            <Routes>
+                <Route exact path='/' element={<SelecaoFilme />} />
+            </Routes>
+        </BrowserRouter>
     )
 }
 
